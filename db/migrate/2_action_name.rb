87eb70f3175cb8e585c569c43d1f8306b0404cf6
Samuel Clopton <samuel.clopton@server.local>
2013-11-18 15:13:41 -0800

    forgot to add name field to actions table

---
+++ db/migrate/2_action_name.rb
@@ -0,0 +1,9 @@
+class ActionName < ActiveRecord::Migration
+	def up
+		add_column :notify_me_actions, :name, :string
+	end
+
+	def down
+		remove_column :notify_me_actions, :name, :string
+	end
+end
